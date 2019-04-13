896800d219ce1b7beb9df2d2b6d6395e41a4570e
karway211 <karway211@gmail.com>
2019-04-13 15:18:16 +0300

    tasks

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,54 @@
-function test() {
-    var eng = ['green', 'yellow', 'red', 'black'];
-    var rus = ['зеленый', 'желтый', 'красный', 'черный'];
+/*var arr = ['red', 'orange', 'yellow', 'green', 'blue', 'navi-blue', 'purple']
 
-    do {
-        var index = Math.floor(Math.random() * (eng.length - 1));
-        var answer = prompt(eng[index]);
-        if(rus[index] == answer) {
-            alert('You are cool guy!')
-        } else {
-            alert('Go to scoll!')
-        }
-    } while (confirm('continue?'));
+for (var i=0; i<=6; i++) {
+    alert(arr[i]);
+}
+for (var i=6; i>=0; i--) {
+    alert(arr[i]);
+}  в обратном порядке
+
+for (var i=0; i<=6; i+=2) {
+    alert(arr[i]); 
+}  четные
+for (var i=1; i<=6; i+=2) {
+    alert(arr[i]); 
+} нечет
+for (var i=0; i<=6; i+=2) {
+    break;
+    alert(arr[i]); 
+} выход из цикла
+for (var i=6; i>=0; i--) {
+    if(i == 4){
+        continue;
+    }
+    if(i == 1){
+        break;
+    }
+    alert(arr[i]);
+} пропустить 4-й и закончить на первом*/
+                    /*заполнение анкеты*/
+//var questions = ['First name','Last nane', 'Gender', 'Age'];
+//var answers = [];
+
+//for (var i = 0; i < questions.length/*длина массива*/; i++){
+//    answers[i] = prompt('Введите ' + questions[i]);
+//}
+//alert(answers);  
+
+function Fuctorial(num) {
+    if (num > 1) {
+        return num * Fuctorial(num - 1);
+    } else {
+        return 1;
+    }
+}
+console.log(Fuctorial(3)) /*расчет факториала(рекурсия)*/
+
+
+
+var result = 1;
+for (var i = 1; i >= 3; i++) {
+    result = result * 3;
 }
 
-test();
+console.log(result);/*расчет факториала(рекурсия)*/
