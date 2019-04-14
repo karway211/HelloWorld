5230430a65e50e9d768728ac490f85b5d7636fdb
Andrey <karway211@gmail.com>
2019-04-14 19:28:52 +0300

    square of stars

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,6 @@
-function test() {
-    var eng = ['green', 'yellow', 'red', 'black'];
-    var rus = ['зеленый', 'желтый', 'красный', 'черный'];
-
-    do {
-        var index = Math.floor(Math.random() * (eng.length - 1));
-        var answer = prompt(eng[index]);
-        if(rus[index] == answer) {
-            alert('You are cool guy!')
-        } else {
-            alert('Go to scoll!')
+    for(i=0; i<5; i++) {
+        for(j=0; j<4; j++) {
+            document.write('* ');
         }
-    } while (confirm('continue?'));
-}
-
-test();
+        document.write('*'+'<br/>');
+    }
